4cccb3f4f3f22965ac9e00f33d99f6f95bf4cbba
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-11 14:14:06 -0800

    Updated gitignore and added questions to the inquirier prompt

--- index.js
+++ index.js
@@ -2,7 +2,47 @@ const inquirer = require('inquirer');
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+      }, 
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+      }, 
+      { 
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information.'
+      },
+      {
+        type: 'input', 
+        name: 'contribution',
+        message: 'Please provide contribution guidelines.'
+      },
+      {
+        type: 'input', 
+        name: 'test',
+        message: 'Please provide test instructions.'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license.',
+        choices: ['MIT', 'APACHE-2.0', 'BSD-3-Clause', 'BSD-2-Clause', 'GPL', 'MPL-2.0', 'None']
+      }, 
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+      }
   ])
   .then(answers => {
     // Use user feedback for... whatever!!
